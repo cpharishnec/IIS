2a63512799afec225ff3b49f8baa50c842762a07
cpharishnec <harish.p@8kmiles.com>
2015-09-03 16:22:05 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,13 @@
 class iis {
-  iis::manage_app_pool {'www.internalapi.co.uk':
+  iis::manage_app_pool {'www.lawfirm.in':
     enable_32_bit           => true,
     managed_runtime_version => 'v4.0',
   }
 
-  iis::manage_site {'www.internalapi.co.uk':
-    site_path   => 'C:\inetpub\wwwroot\test',
+  iis::manage_site {'Default Web Site':
+    site_path   => 'C:\lawfirm\lawfirm',
     port        => '80',
     ip_address  => '*',
-    host_header => 'www.internalapi.co.uk',
-    app_pool    => 'www.internalapi.co.uk'
-  }
-
-  iis::manage_virtual_application {'reviews':
-    site_name => 'www.internalapi.co.uk',
-    site_path => 'C:\inetpub\wwwroot\test',
-    app_pool  => 'www.internalapi.co.uk'
+    app_pool    => 'www.lawfirm.in'
   }
 }
